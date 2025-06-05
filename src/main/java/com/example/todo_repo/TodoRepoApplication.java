1f0f919e779eed52d2d4699f78c666bbccaaedd1
sam <samuzawayu@cy-com.co.jp>
2025-06-05 13:41:01 +0900

    feat pom 6/5

---
+++ src/main/java/com/example/todo_repo/TodoRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRepoApplication.class, args);
+	}
+
+}
