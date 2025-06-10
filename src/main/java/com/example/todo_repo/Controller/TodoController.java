2e204d14be346728958405d903d1f124c7d9bde0
sam <samuzawayu@cy-com.co.jp>
2025-06-10 17:10:01 +0900

    feat all files
    Co-authored-by: 小松堅登 <komatsuke@cy-com.co.jp>
    Co-authored-by: miyazaki-cy <miyazaki-cy@users.noreply.github.com>

--- src/main/java/com/example/todo_repo/Controller/TodoController.java
+++ src/main/java/com/example/todo_repo/Controller/TodoController.java
@@ -1,41 +1,58 @@
 package com.example.todo_repo.controller;
- import com.example.todo_repo.entity.Todo;
+
+import com.example.todo_repo.entity.Todo;
 import com.example.todo_repo.form.TodoForm;
 import com.example.todo_repo.service.TodoService;
- import org.springframework.stereotype.Controller;
- import org.springframework.ui.Model;
- import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
- import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
- @Controller
- @RequestMapping("/todos")
- public class TodoController {
+@Controller
+@RequestMapping("/todos")
+public class TodoController {
     private final TodoService todoService;
 
     public TodoController(TodoService todoService) {
-       this.todoService = todoService;
-   }
+        this.todoService = todoService;
+    }
 
     @GetMapping
-   public String todos(Model model) {
-       List<Todo> todos = todoService.getAllTodos();
-       model.addAttribute("todos", todos);
-       return "todo/todo-list";
-   }
-   @GetMapping("/new")
-   public String todoForm(Model model) {
-       TodoForm todoForm =  new TodoForm();
-       return "todo/toddo-list";
-   }
+    public String todos(Model model) {
+        List<Todo> todos = todoService.getAllTodos();
+        model.addAttribute("todos", todos);
+        return "todo/todo-list";
+    }
+
+    @GetMapping("/new")
+    public String todoForm(Model model) {
+        TodoForm todoForm = new TodoForm();
+        return "todo/todo-list";
+    }
+
    @PostMapping("/new")
    public String createTodo(TodoForm todoForm) {
        todoService.createTodo(todoForm);
-       return "redirect:/todos";
+       return "redirect:/todo";
    }
-   @GetMapping
-   public String todo(@PathVariable long, Model model) {  //途中です
-    
+
+   @GetMapping("/{todoId}")
+   public String todo(@PathVariable long taskId, Model model) {  
+    Todo todo = todoService.getTodoById(taskId);
+    List//途中
    }
+
+    @PostMapping("/{todoId}/delete")
+    public String deleteTodo(@PathVariable long taskId) {
+        todoService.deleteTodo(taskId);
+        return "redirect:/todo";
+    }
+
+    public String createTaskForm(@PathVariable long taskId, TaskForm taskForm) {
+        //途中です
+    }
+
  }
--- src/main/java/com/example/todo_repo/security/SecurityConfig.java
+++ src/main/java/com/example/todo_repo/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.example.todo_repo.security;
  import org.springframework.context.annotation.Bean;
  import org.springframework.context.annotation.Configuration;
  import org.springframework.security.config.annotation.web.builders.HttpSecurity;
- import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import java.security.Security;
 
 @Configuration
@@ -22,5 +24,10 @@ public class SecurityConfig {
                 .permitAll()
             );
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
