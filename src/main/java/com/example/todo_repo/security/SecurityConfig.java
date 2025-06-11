1448b4c7d3709c4358a50e195c2b865e6937c4d2
sam <samuzawayu@cy-com.co.jp>
2025-06-11 13:37:47 +0900

    feat securityConfig

--- src/main/java/com/example/todo_repo/security/SecurityConfig.java
+++ src/main/java/com/example/todo_repo/security/SecurityConfig.java
@@ -1,33 +1,34 @@
-// package com.example.todo_repo.security;
+package com.example.todo_repo.security;
 
-//  import org.springframework.context.annotation.Bean;
-//  import org.springframework.context.annotation.Configuration;
-//  import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.web.SecurityFilterChain;
-// import java.security.Security;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import java.security.Security;
 
-// @Configuration
-// public class SecurityConfig {
+@Configuration
+public class SecurityConfig {
 
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-//         http 
-//             .authorizeHttpRequests(request -> request
-//                 .anyRequest().authenticated())
-//             .formLogin(login -> login
-//                 .loginProcessingUrl("/login")
-//                 .loginPage("/login")
-//                 .defaultSuccessUrl("/todos")
-//                 .failureUrl("/login?error")
-//                 .permitAll()
-//             );
-//         return http.build();
-//     }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+    .authorizeHttpRequests(request -> request
+        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+        .anyRequest().authenticated())
+    .formLogin(login -> login
+        .loginProcessingUrl("/login")
+        .loginPage("/login")
+        .defaultSuccessUrl("/todos")
+        .failureUrl("/login?error")
+        .permitAll()
+    );
+        return http.build();
+    }
 
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
